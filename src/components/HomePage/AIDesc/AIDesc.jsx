629acd69ea015786e0431729c3a117c4d3a8ec37
Nmait2111 <namitjain2111@gmail.com>
2023-04-14 14:25:58 +0530

    padding-changed

--- src/components/HomePage/AIDesc/AIDesc.jsx
+++ src/components/HomePage/AIDesc/AIDesc.jsx
@@ -7,6 +7,7 @@ const AIDesc = () => {
       <div className='AIDesc'>
       <div className='AICont'>
         <p>All variety of AI tools to make your work easy</p>
+        {/* <button id="AiTools">View Tools</button> */}
       </div>
 
     <div className='AIImage'><img src={require("../../../Assets/Images/AiTools.png")} alt="Tools"/></div>
